d2a56d157f5b94dead79b32f56ae58d186129a05
d.smakovskiy <smakovskiy@ukr.net>
2020-12-17 12:40:36 +0200

    initial commit

---
+++ src/main/java/ua/kpi/dbsample/DbsampleApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.dbsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kpi/dbsample/DbsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.dbsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
